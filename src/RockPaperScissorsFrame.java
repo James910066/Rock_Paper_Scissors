c30bb61f11478a88f8f8ead3f94a693ba496bc37
James910066 <95539722+James910066@users.noreply.github.com>
2023-10-02 19:33:00 -0400

    Update RockPaperScissorsFrame.java

--- src/RockPaperScissorsFrame.java
+++ src/RockPaperScissorsFrame.java
@@ -4,6 +4,7 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class RockPaperScissorsFrame extends JFrame implements ActionListener
 {
@@ -29,7 +30,9 @@ public class RockPaperScissorsFrame extends JFrame implements ActionListener
 
     public RockPaperScissorsFrame()
     {
-        // Center Game on screen
+        /**
+         * Center Game on screen
+          */
         Toolkit kit = Toolkit.getDefaultToolkit();
         Dimension screenSize = kit.getScreenSize();
         int screenHeight = screenSize.height;
@@ -37,10 +40,16 @@ public class RockPaperScissorsFrame extends JFrame implements ActionListener
         setSize(screenWidth / 2, screenHeight / 2);
         setLocation(screenWidth / 4, screenHeight / 4);
 
+        /**
+         * Create main panel
+         */
         mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
 
-        // Create icon panel
+        /**
+         * Create icon panel and set icon image
+         * Create and position the title labels
+          */
         iconPnl = new JPanel();
         icon = new ImageIcon("src/RPS.jpg");
         iconPnl.setSize(20, 20);
@@ -49,10 +58,16 @@ public class RockPaperScissorsFrame extends JFrame implements ActionListener
         titleLbl.setHorizontalTextPosition(JLabel.CENTER);
         iconPnl.add(titleLbl);
 
-        // Add icon panel to main panel
+        /**
+         * Add icon panel to main panel
+         */
         mainPanel.add(iconPnl, BorderLayout.NORTH);
 
-        // Create display panel
+        /**
+         *Create display panel, text Areas and scroller for the Window that shows the game results as it is played
+         * Border is also added to the panel.
+         */
+
         displayPnl = new JPanel();
         displayTA = new JTextArea(20, 86);
         displayTA.setEditable(false);
@@ -62,26 +77,38 @@ public class RockPaperScissorsFrame extends JFrame implements ActionListener
         displayBorder.setTitleJustification(TitledBorder.CENTER);
         displayPnl.setBorder(displayBorder);
 
-        // Add display panel to main panel
+        /**
+         * Add display panel to main panel
+         */
         mainPanel.add(displayPnl, BorderLayout.WEST);
 
-        // Create stats panel
+        /**
+         *  Create stats panel on the right side of main panel. Add in border with title of the panel
+          */
+
         createStatsPanel();
         mainPanel.add(statsPanel, BorderLayout.EAST);
         TitledBorder border = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Game Stats");
         border.setTitleJustification(TitledBorder.CENTER);
         statsPanel.setBorder(border);
 
-        // Create control panel
+        /**
+         * Create control panel and add it to the main panel. Set the size and add title.
+          */
         createControlPanel();
         mainPanel.add(controlPnl, BorderLayout.SOUTH);
-
         add(mainPanel);
         setSize(800, 800);
         setTitle("Rock-Paper-Scissors");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
+
+    /**
+     * Method for creating the stats panel that displays the win and tie counts for both players.
+     * Add the labels to the stats panel
+     * Create labels for the stats panel. These track the wins and ties
+     */
     private void createStatsPanel()
     {
         statsPanel = new JPanel();
@@ -100,6 +127,12 @@ public class RockPaperScissorsFrame extends JFrame implements ActionListener
         statsPanel.add(tie);
         statsPanel.setVisible(true);
     }
+
+    /**
+     * Method for creating the control panel with a title and border.
+     * This also contains all the buttons to run the game
+     * All the icon images are incorporated here as well
+     */
     private void createControlPanel()
     {
         controlPnl = new JPanel();
@@ -127,6 +160,11 @@ public class RockPaperScissorsFrame extends JFrame implements ActionListener
         controlPnl.add(scissorsBtn);
         controlPnl.add(quitBtn);
     }
+
+    /**
+     * Method for each button action
+     * @param ae the event to be processed
+     */
     public void actionPerformed(ActionEvent ae)
     {
         if (ae.getSource() == rockBtn)
@@ -142,6 +180,11 @@ public class RockPaperScissorsFrame extends JFrame implements ActionListener
             playGame("scissors");
         }
     }
+
+    /**
+     * Method for checking who won and accumulating the win count.
+     * @param playerChoice
+     */
     private void playGame(String playerChoice)
     {
         String computerChoice = generateComputerChoice();
@@ -163,6 +206,13 @@ public class RockPaperScissorsFrame extends JFrame implements ActionListener
         cWin.setText("Computer Wins: " + computerWinCnt);
         tie.setText("Tie Games: " + (playerWinCnt + computerWinCnt - 1));
     }
+
+    /**
+     * Method for determining a winner or a tie game
+     * @param playerChoice
+     * @param computerChoice
+     * @return
+     */
     private String determineWinner(String playerChoice, String computerChoice)
     {
         if (playerChoice.equals(computerChoice))
@@ -180,8 +230,13 @@ public class RockPaperScissorsFrame extends JFrame implements ActionListener
             return "Computer wins!";
         }
     }
+    /**
+     * Method that uses random function to simulate computer player by randomly select choices from the array using index
+     * @return
+     */
     private String generateComputerChoice()
     {
+        Random rnd = new Random();
         String[] choices = {"rock", "paper", "scissors"};
         int randomIndex = (int) (Math.random() * choices.length);
         return choices[randomIndex];
